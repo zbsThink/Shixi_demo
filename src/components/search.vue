fec3f03a32985eaed6101f5c3843e2878918d932
Zeng.Baosheng <Baosheng.Zeng@desay-svautomotive.com>
2019-09-23 18:45:35 +0800

    add search.vue Zeng.Baosheng

--- src/components/search.vue
+++ src/components/search.vue
@@ -12,12 +12,22 @@ export default {
     data(){
         return {
            searchValue:'',
-           inputSuggestion:[]
+           inputSuggestions:[{"value":"三鲜小龙虾","address":"上海五福路"},
+           { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+          { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
+          { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" }]
         }
     },
     methods:{
-      querySearch(queryString){
-          
+      querySearch(queryString,cb){
+         var inputSuggestions = this.inputSuggestions;
+         var result = queryString ? inputSuggestions.filter(this.createFilter(queryString)) : inputSuggestion;
+         cb(result)
+      },
+      createFilter(queryString){
+         return (inputSuggestion) => {
+            return inputSuggestion.value.toLowerCase().indexOf(queryString) !== -1
+         } 
       }
     }
 }
